726e777db99f7e83d439ba291b7e5e83080d5a20
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-01-25 16:37:09 +0100

    added current contact & created get avatar url function

--- js/script.js
+++ js/script.js
@@ -4,18 +4,23 @@ const app = createApp({
     name: 'Boolzapp',
     data: () => ({
         user: data.user,
-        contacts: data.contacts
+        contacts: data.contacts,
+        activeId: data.contacts.currentContact
     }),
 
     computed: {
-
+        currentContact() {
+        return  this.contacts.reduce((lowest, contact) => contact.id < lowest.id ? contact : lowest, this.contacts[0])
+        }
     },
 
     methods: {
+        getAvatarUrl({avatar}) {
+            return `img/avatar${avatar}.jpg`
+        }
 
     }
 });
 
 app.mount('#root');
 
-console.log(data)
